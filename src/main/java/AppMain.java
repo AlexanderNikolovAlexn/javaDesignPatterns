49f13c0f6e4f326f29134a1379a2d241b965bde0
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2019-01-06 12:57:42 +0200

    Init empty project - Hello World!

---
+++ src/main/java/AppMain.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Alexander Nikolov on 6.1.2019 г..
+ */
+public class AppMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
